f596ada9d1ea7fe15eed03662ef2acd1ffa0a4f2
jecombe <jeremcombe@hotmail.fr>
2024-09-18 00:50:39 +0200

    change var

--- smartContract/contracts/EncryptedErc721.sol
+++ smartContract/contracts/EncryptedErc721.sol
@@ -11,7 +11,6 @@ contract EncryptedErc721 is Ownable, ERC721 {
 
     constructor(string memory name, string memory symbol) Ownable(msg.sender) ERC721(name, symbol) {}
 
-    // Fonction pour récupérer la clé chiffrée
     function getKeys(uint256 tokenId) public view returns (euint64) {
         require(ownerOf(tokenId) == msg.sender, "ERC721: caller is not the owner of this token");
         return eKeys[msg.sender][tokenId];
@@ -21,24 +20,25 @@ contract EncryptedErc721 is Ownable, ERC721 {
         return TFHE.randEuint64();
     }
 
-    // Fonction pour frapper un nouveau NFT
-    function mint(address to, uint256 tokenId, string memory tokenURI, einput _eKey, bytes calldata inputProof) public {
+    function mint(
+        address to,
+        uint256 tokenId,
+        string memory _UriToken,
+        einput _eKey,
+        bytes calldata inputProof
+    ) public {
         euint64 eKey = TFHE.asEuint64(_eKey, inputProof);
         eKeys[to][tokenId] = eKey;
-        _setTokenURI(tokenId, tokenURI, eKey);
+        _setTokenURI(tokenId, _UriToken, eKey);
         _mint(to, tokenId);
     }
 
-    // Fonction pour définir l'URI d'un token
     function _setTokenURI(uint256 tokenId, string memory _tokenURI, euint64 _eKey) internal {
-        // require(_exists(tokenId), "ERC721Metadata: URI set of nonexistent token");
         _tokenURIs[tokenId] = _tokenURI;
         eKeys[msg.sender][tokenId] = _eKey;
     }
 
-    // Fonction pour obtenir l'URI d'un token
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        //   require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
         return _tokenURIs[tokenId];
     }
 }
